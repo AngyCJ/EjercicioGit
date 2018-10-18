555bc1b36ead014d8648e1bf168cd33f2be9eb47
Edgar Ortiz Loyola <virtualizacion07@virtualizacions-imac.local>
2018-10-18 18:43:45 -0500

    Outlets y funciones agregadas

--- EjercicioGit/ViewController.swift
+++ EjercicioGit/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelTexto: UILabel!
+    @IBOutlet weak var botonAgregar: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func Agregar() {
+    }
+    
 
 }
 
